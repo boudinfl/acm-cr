6b4dc34221b05cf6a512b5c7914e60fcb1975d74
Florian Boudin <florian.boudin@univ-nantes.fr>
2020-10-09 15:19:49 +0200

    adding some articles

--- src/bib_to_trec.py
+++ src/bib_to_trec.py
@@ -9,7 +9,7 @@ from pybtex.database.input import bibtex
 
 collection = {}
 
-for filename in glob.iglob('data/**', recursive=True):
+for filename in glob.iglob('data/acm-dl/**', recursive=True):
     if os.path.isfile(filename):
         print('loading documents from {}'.format(filename))
         parser = bibtex.Parser()
@@ -20,6 +20,14 @@ for filename in glob.iglob('data/**', recursive=True):
                 continue
             if {'title', 'abstract', 'keywords'} <= set(reference.fields):
                 collection[key] = reference
+
+            # dd url
+            url = "https://dl.acm.org/doi/pdf/"+key
+
+            print(key)
+            print(url)
+
+            exit(0)
 
         print('{} documents in total'.format(len(collection)))
 
