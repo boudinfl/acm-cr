4e72340d51631ed3ba8c4464635a164cef023047
Florian Boudin <florian.boudin@univ-nantes.fr>
2020-10-12 17:08:45 +0200

    bibtex fix + add SIGMOD - now at 26K+ docs

--- src/bib_to_trec.py
+++ src/bib_to_trec.py
@@ -9,16 +9,20 @@ from pybtex.database.input import bibtex
 
 collection = {}
 root_path = 'data/acm-dl/**'
+nb_entries = 0
 
 for filename in glob.iglob(root_path, recursive=True):
     if os.path.isfile(filename):
         print('loading documents from {}'.format(filename))
         parser = bibtex.Parser()
         data = parser.parse_file(filename)
+        nb_docs = 0
 
         for key, reference in  data.entries.items():
-            if reference.type != "inproceedings":
+            nb_docs += 1
+            if reference.type not in ["inproceedings", "article"]:
                 continue
+
             if {'title', 'abstract', 'keywords'} <= set(reference.fields):
                 #collection[key] = reference
                 collection[key] = {}
@@ -31,6 +35,9 @@ for filename in glob.iglob(root_path, recursive=True):
 
             #exit(0)
 
-        print('{} documents in total'.format(len(collection)))
+        nb_entries += nb_docs
+
+print('{} entries in total'.format(nb_entries))
+print('{} documents in total'.format(len(collection)))
 
 
